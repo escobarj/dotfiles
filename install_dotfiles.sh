37e732dd0b9cf2c75ebd797a16d3e6541bc1e4fc
Jorge Escobar <escobar6@llnl.gov>
2015-06-11 10:27:09 -0700

    fixes

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -3,20 +3,29 @@
 # This script creates symlinks from the home directory dotfiles in ~/dotfiles
 ############################
 
-dotfiles_dir=~/dotfiles/home
-dotfiles_backup_dir=~/.dotfiles_backup
+CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+dotfiles_dir="${CWD}/home"
+dotfiles_backup_dir="${HOME}/.dotfiles_backup"
+
+# Debug
+# echo $dotfiles_dir
+# echo $dotfiles_backup_dir
+# exit
 
 # create dotfiles_old in homedir
 if [[ ! -d $dotfiles_backup_dir ]]; then
 	mkdir -p $dotfiles_backup_dir
 fi
 
-# move any existing dotfiles in homedir to dotfiles.bak directory, then create symlinks 
-for file in "${dotfiles_dir}/*"; do
-	name=$(basename $file)
-	# echo ~/.${name} $dotfiles_backup_dir
-	if [[ -f ~/.${name} ]]; then
-		mv ~/.${name} $dotfiles_backup_dir/${name}
-	fi
-    ln -s $file ~/.${name}
+# create a backup of existing dotfile and then create symlink to new dotfile 
+for dfile in "${dotfiles_dir}/*"; do
+		fname=$(basename $dfile)
+		current_file="${HOME}/.${fname}"
+		if [[ -f "$current_file" ]]; then
+			echo "Backing up" $current_file
+			cp "$current_file" "${dotfiles_backup_dir}/${fname}_$(date "+%Y.%m.%d-%H.%M.%S")"
+			rm $current_file
+		fi
+		echo "Linking" $dfile
+	    ln -s $dfile "$current_file"
 done
