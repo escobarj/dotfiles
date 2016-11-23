340ae524922e02837ae59b3d8da18a40f770f237
Jorge Escobar <escobarj@gmail.com>
2016-11-23 11:25:01 -0800

    Remove unused binaries and add brew cask apps

--- brew.sh
+++ brew.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+
+# Install brew if needed
+if ! [[ -f "/usr/local/bin/brew" ]]; then
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -8,65 +12,46 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-# brew install coreutils
-
-# Install some other useful utilities like `sponge`.
-# brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-# brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-# brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-# brew install bash
-# brew tap homebrew/versions
-# brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-# if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-#   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-#   chsh -s /usr/local/bin/bash;
-# fi;
-
-
-# Install more recent versions of some macOS tools.
-# brew install vim --override-system-vi
-# brew install homebrew/dupes/grep
-# brew install homebrew/dupes/openssh
-# brew install homebrew/dupes/screen
-# brew install homebrew/php/php56 --with-gmp
-
-
-
-# Install other useful binaries.
-# brew install ack
-# brew install dark-mode
-#brew install exiv2
-# brew install git
-# brew install git-lfs
-# brew install imagemagick --with-webp
-# brew install lua
-# brew install lynx
-# brew install p7zip
-# brew install pigz
-# brew install pv
-# brew install rename
-# brew install rhino
-# brew install speedtest_cli
+# Misc binaries
 brew install ssh-copy-id
-# brew install testssl
 brew install tree
-# brew install vbindiff
-# brew install webkit2png
-# brew install zopfli
-
-# My brew packages
+brew install wget
 brew install shellcheck
 brew install youtube-dl
 
+# Brew cask apps
+brew tap caskroom/cask
+brew cask install sublime-text
+brew cask install atom
+brew cask install pycharm
+brew cask install sourcetree
+brew cask install google-chrome
+brew cask install firefox
+brew cask install caffeine
+brew cask install coconutbattery
+brew cask install flux
+brew cask install grandperspective
+brew cask install hermes
+brew cask install mysqlworkbench
+brew cask install sequel-pro
+brew cask install mysql-connector-python
+brew cask install platypus
+brew cask install sqlitebrowser
+brew cask install vlc
+
+# for home computer
+brew cask install microsoft-office
+brew cask install virustotaluploader
+brew cask install dropbox
+brew cask install private-internet-access
+brew cask install ripit
+brew cask install handbrake
+brew cask install atext
+brew cask install airflow
+
+# Brew python packages are handled in python.sh
+
+brew linkapps
 
 # Remove outdated versions from the cellar.
 brew cleanup
